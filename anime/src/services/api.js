a9a5f10450cb8abfc47580bb32f48d8a5998e61e
nursbaron1 <nursultankalkamanov@gmail.com>
2025-11-12 15:37:06 +0500

    Update api.js

--- anime/src/services/api.js
+++ anime/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://js-course.vercel.app/api';
 
 // Жалпы API функциясы
 const apiRequest = async (endpoint, options = {}) => {
